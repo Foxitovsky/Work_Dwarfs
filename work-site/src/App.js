9e72be74ac1ff703e2d1bfb2d85dbcf2b66fce8c
d.poshekhontsev <d.poshekhntsev@protobase.ru>
2024-05-03 15:56:01 +0300

    add form

---
+++ work-site/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+        <h1>Сайт в разработке</h1>
+        <p>Мы работаем над улучшением нашего сайта. Скоро мы вернемся!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
